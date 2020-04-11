60c6e232ab11e97e921bb653612a78bcbd175374
formix <jeanphilippe.gravel@gmail.com>
2020-04-11 16:53:47 -0400

    added the IS_ROW_COMPLETE function

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p out
-zasm -uyw tetriz.z80 -o out/a.out
+zasm -uyw tetriz.z80 -o out/tetriz.bin
 if [ $? -eq 0 ]; then
-    cat out/a.out > /dev/ttyUSB0
+    cat out/tetriz.bin > /dev/ttyUSB0
 else
     >&2 echo "BUILD FAILED"
 fi
