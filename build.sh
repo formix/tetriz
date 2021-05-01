017a13cd33838c7347ee5f781417b16be5ca565e
formix <jeanphilippe.gravel@gmail.com>
2021-05-01 19:00:36 -0400

    change layout

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir -p out
-zasm -uyw core.z80 -o out/tetriz.bin   
+zasm -uyw main.z80 -o out/tetriz.bin   
 if [ $? -eq 0 ]; then
     cat out/tetriz.bin > /dev/ttyUSB0
 else
-    >&2 echo "BUILD OF core.z80 FAILED"
+    >&2 echo "BUILD OF main.z80 FAILED"
 fi
 
